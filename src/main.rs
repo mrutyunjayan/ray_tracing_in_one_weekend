a43ad105314e672dd115a7d39d19dddc0049409c
Jaikrishnan Arun <jaikrishnanarun@hotmail.com>
2020-05-21 18:21:28 -0700

    a

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use lib::{
 };
 
 use rand::prelude::*;
-//use std::f64::consts::*;
+use std::time;
 
 fn ray_color(ray: &Ray, world: &dyn Hittable, depth: u16) -> Color {
     let mut hit_rec = HitRecord::new_invalid();
@@ -50,36 +50,34 @@ fn render(aspect_ratio: f64, image_height: usize, samples_per_pixel: usize, max_
     let mut world: HittableList = HittableList::new();
 
     world.add(Sphere::new_hittable(
-        Point3::new(0.0, 0.0, -1.0),
-        0.5,
-        Material::lambertian(&Color::new(0.1, 0.2, 0.5)),
+        Point3::new(0.0, -1000.0, -1.0),
+        1000.0,
+        Material::lambertian(&Color::new(0.5, 0.5, 0.5)),
+    ));
+
+    world.add(Sphere::new_hittable(
+        Point3::new(-4.0, 1.0, 0.0),
+        1.0,
+        Material::lambertian(&Color::new(0.4, 0.2, 0.1)),
     ));
     world.add(Sphere::new_hittable(
-        Point3::new(0.0, -100.5, -1.0),
-        100.0,
-        Material::lambertian(&Color::new(0.8, 0.8, 0.0)),
+        Point3::new(4.0, 1.0, 0.0),
+        1.0,
+        Material::metal(&Color::new(0.7, 0.6, 0.5), 0.0),
     ));
     world.add(Sphere::new_hittable(
-        Point3::new(1.0, 0.0, -1.0),
-        0.5,
-        Material::metal(&Color::new(0.8, 0.6, 0.2), 0.0),
-    ));
-    world.add(Sphere::new_hittable(
-        Point3::new(-1.0, 0.0, -1.0),
-        0.5,
-        Material::dielectrtic(1.5),
-    ));
-    world.add(Sphere::new_hittable(
-        Point3::new(-1.0, 0.0, -1.0),
-        -0.45,
+        Point3::new(0.0, 1.0, 0.0),
+        1.0,
         Material::dielectrtic(1.5),
     ));
 
+    make_random_spheres(&mut world);
+
     let v_up = Vec3::new(0.0, 1.0, 0.0);
-    let look_from = Point3::new(3.0, 3.0, 2.0);
-    let look_at = Point3::new(0.0, 0.0, -1.0);
-    let focus = (look_from - look_at).length();
-    let aperture = 0.2;
+    let look_from = Point3::new(13.0, 2.0, 3.0);
+    let look_at = Point3::new(0.0, 0.0, 0.0);
+    let focus = 10.0;
+    let aperture = 0.1;
     let v_fov = 20.0;
 
     let cam = Camera::new(
@@ -126,16 +124,60 @@ fn render(aspect_ratio: f64, image_height: usize, samples_per_pixel: usize, max_
     eprintln!("\nDone\n");
 }
 
+fn make_random_spheres(world: &mut lib::hittable_list::HittableList) {
+    let mut rng = rand::thread_rng();
+    let mut center: Vec3;
+    let mut albedo: Color;
+    let mut fuzz: f64;
+    let radius = 0.2;
+
+    for a in -11..11 {
+        let choose_mat = rng.gen::<f64>();
+        for b in -11..11 {
+            center = Point3::new(a as f64, 0.2, b as f64 + 0.9 * rng.gen::<f64>());
+            if (center - Vec3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                if choose_mat < 0.8 {
+                    //diffuse
+                    albedo = &Color::random() * &Color::random();
+                    world.add(Sphere::new_hittable(
+                        center,
+                        radius,
+                        Material::lambertian(&albedo),
+                    ));
+                } else if choose_mat < 0.95 {
+                    albedo = Color::new(rng.gen::<f64>(), 0.5, 1.0);
+                    fuzz = rng.gen::<f64>() * 0.5;
+                    world.add(Sphere::new_hittable(
+                        center,
+                        radius,
+                        Material::metal(&albedo, fuzz),
+                    ));
+                } else {
+                    world.add(Sphere::new_hittable(
+                        center,
+                        radius,
+                        Material::dielectrtic(1.5),
+                    ))
+                }
+            }
+        }
+    }
+}
+
 fn main() {
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
-    const IMAGE_HEIGHT: usize = 1080;
+    const IMAGE_HEIGHT: usize = 144;
     const IMAGE_WIDTH: usize = (IMAGE_HEIGHT as f64 * ASPECT_RATIO) as usize;
     const SAMPLE_PER_PIXEL: usize = 100;
     const MAX_DEPTH: u16 = 50;
 
+    let start = time::Instant::now();
+
     render(ASPECT_RATIO, IMAGE_HEIGHT, SAMPLE_PER_PIXEL, MAX_DEPTH);
+
+    let duration = time::Instant::now() - start;
     eprintln!(
-        "Rendered image with dimensions:\n {} x {}",
-        IMAGE_WIDTH, IMAGE_HEIGHT
+        "Rendered image with dimensions:\n {} x {}\n in {:?} seconds",
+        IMAGE_WIDTH, IMAGE_HEIGHT, duration
     );
 }
